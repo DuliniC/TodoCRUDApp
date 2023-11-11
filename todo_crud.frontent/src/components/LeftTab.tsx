b2bc487bda534c9d89c066b64874defe26586784
DuliniC <dchamathka9@gmail.com>
2023-11-11 21:06:59 +0530

    Get All API call and update API for complete status-frontend

--- todo_crud.frontent/src/components/LeftTab.tsx
+++ todo_crud.frontent/src/components/LeftTab.tsx
@@ -2,9 +2,42 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
-import PendingTodoView from './PendingTodoView';
+import TodoCard from './TodoCard';
+import { useEffect, useState } from 'react';
+import { TodoTask } from '../models/TodoTaskModel';
+import API_BASE_URL from '../config';
+import Button from 'react-bootstrap/esm/Button';
 
-const LeftTabsExample : React.FC = () => {
+const LeftTabsExample = () => {
+  const [todoList, setTodoList] = useState<TodoTask[]>([]);
+  const pendingTodoList = todoList.filter((todo) => !todo.isCompleted);
+  const completedTodoList = todoList.filter((todo) => todo.isCompleted);
+
+    useEffect(() => {
+        //GetAll API call
+        const getAllTodos = async () => {
+            try {
+              const response = await fetch(`${API_BASE_URL }/api/todo-tasks`,{
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'text/plain',
+                }
+              }); 
+              if (response.ok) {
+                const todoData: TodoTask[] = await response.json();
+                setTodoList(todoData);
+              } else {
+                console.error('Failed to get todo list');
+              }
+            } catch (error) {
+              console.error('Error:', error);
+            }
+          };
+       
+          getAllTodos();
+    }, [])
+
+    
   return (
     <Tab.Container id="todo-left-tabs" defaultActiveKey="pending-list">
       <Row>
@@ -20,8 +53,11 @@ const LeftTabsExample : React.FC = () => {
         </Col>
         <Col sm={10}>
           <Tab.Content>
-            <Tab.Pane eventKey="pending-list"><PendingTodoView/></Tab.Pane>
-            <Tab.Pane eventKey="completed-list">Second tab content</Tab.Pane>
+            <Tab.Pane eventKey="pending-list">
+              <Button variant="primary" style={{margin: '10px'}}>Add New Task</Button>
+              <TodoCard todo={pendingTodoList}/>
+            </Tab.Pane>
+            <Tab.Pane eventKey="completed-list"><TodoCard todo={completedTodoList}/></Tab.Pane>
           </Tab.Content>
         </Col>
       </Row>
--- todo_crud.frontent/src/components/TodoCard.tsx
+++ todo_crud.frontent/src/components/TodoCard.tsx
@@ -1,55 +1,97 @@
-import Card from 'react-bootstrap/Card';
-import { TodoTask } from '../models/TodoTaskModel';
-import { priorities } from '../models/TodoPriorityEnum';
+import Card from "react-bootstrap/Card";
+import { TodoTask } from "../models/TodoTaskModel";
+import { priorities } from "../models/TodoPriorityEnum";
+import * as Icon from "react-bootstrap-icons";
+import { Col, Row } from "react-bootstrap";
+import CheckBox from "./Checkbox";
 
 type TodoProps = {
-    todo : TodoTask[]
-}
+  todo: TodoTask[];
+};
 
-const TodoCard = ({todo}: TodoProps) => {
-    
-    const colors = [
-        {
-            //Default
-            primaryColor : "#5D93E1",
-            secondaryColor : "#ECF3FC"
-        },
-        {
-            //Low Priority
-            primaryColor : "#5DC250",
-            secondaryColor : "#F2FAF1"
-        },
-        {
-            //Medium Priority
-            primaryColor : "#F9D288",
-            secondaryColor : "#FEFAF1"
-        },        
-        {
-            //High Priority
-            primaryColor : "#F48687",
-            secondaryColor : "#FDF1F1"
-        }
-    ];
+const TodoCard = ({ todo }: TodoProps) => {
+  const colors = [
+    {
+      //Default
+      primaryColor: "#5D93E1",
+      secondaryColor: "#ECF3FC",
+    },
+    {
+      //Low Priority
+      primaryColor: "#5DC250",
+      secondaryColor: "#F2FAF1",
+    },
+    {
+      //Medium Priority
+      primaryColor: "#F9D288",
+      secondaryColor: "#FEFAF1",
+    },
+    {
+      //High Priority
+      primaryColor: "#F48687",
+      secondaryColor: "#FDF1F1",
+    },
+  ];
 
-    return(
-        <>
-            {todo && todo.map((todo) => (
-                <Card style={{ width: '18rem' }} border={colors[todo.priority].primaryColor}>
-                    <Card.Body>
-                        <Card.Title>{todo.title}</Card.Title>                   
-                        <Card.Text>
-                        {todo.description}
-                        </Card.Text>
-                        <div style={{"position":"absolute",}}>
-                        <div style={{"color": colors[todo.priority].secondaryColor}}>{todo.priority as priorities}</div>
-                        <i className = "bi bi-pencil" style={{"color" : colors[todo.priority].primaryColor, "cursor" : "pointer"}} ></i>
-                        <i className="fas fa-trash-alt" style = {{"color" : colors[todo.priority].primaryColor, "cursor" : "pointer"}} ></i>
-                        </div>
-                    </Card.Body>
-               </Card>
-            ))}
-        </>       
-    );
-}
+  return (
+    <>
+      <Row xs={1} md={2} className="g-3" >
+        {todo &&
+          todo.map((todo, index) => (
+            <Col key={index} xs={10} sm={6} md={4}>
+              <Card
+                style={{
+                  width: "100%",
+                  borderColor: colors[todo.priority].primaryColor,
+                }}
+              >
+                <Card.Header
+                  style={{
+                    borderColor: colors[todo.priority].primaryColor,
+                  }}
+                >
+                  <Row>
+                    <Col>{todo.title}</Col>               
+                  <Col>
+                    <div
+                      style={{
+                        border: `2px solid ${
+                          colors[todo.priority].primaryColor
+                        }`,
+                        backgroundColor: colors[todo.priority].secondaryColor,
+                        textAlign: "center",
+                        borderRadius: "8px",
+                      }}
+                    >   
+                      {priorities[todo.priority]}
+                    </div>
+                  </Col>
+                  <Col>
+                      <CheckBox todo = {todo}/>
+                  </Col>
+                  </Row>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Text>{todo.description}</Card.Text>
+                  <Row>
+                    <Col>
+                      <Icon.PencilFill
+                        color={colors[todo.priority].primaryColor}
+                      />
+                    </Col>
+                    <Col>
+                      <Icon.TrashFill
+                        color={colors[todo.priority].primaryColor}
+                      />
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </>
+  );
+};
 
 export default TodoCard;
