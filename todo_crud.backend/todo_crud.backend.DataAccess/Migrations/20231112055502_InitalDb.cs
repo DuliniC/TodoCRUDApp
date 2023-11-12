cda2eec2ebf5454205afe428dd8d000d6117a306
DuliniC <dchamathka9@gmail.com>
2023-11-12 11:25:29 +0530

    Db change

--- todo_crud.backend/todo_crud.backend.DataAccess/Migrations/20231112055502_InitalDb.cs
+++ todo_crud.backend/todo_crud.backend.DataAccess/Migrations/20231112055502_InitalDb.cs
@@ -5,7 +5,7 @@
 namespace todo_crud.backend.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialDb : Migration
+    public partial class InitalDb : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
